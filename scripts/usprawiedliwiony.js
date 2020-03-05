a88a6030643ef1ac9b80ff1b895cd917b7fe13a3
Karol Z <karol.zientek001@protonmail.com>
2020-03-05 15:17:26 +0100

    Fixed bugs with padding
    Fixed bugs with padding.

--- scripts/usprawiedliwiony.js
+++ scripts/usprawiedliwiony.js
@@ -2,5 +2,7 @@ $(document).ready(function() {
     const img = $(".x-obecnosc img").attr("src")
     
     $(".x-nieobecnosc-nieuspr img").attr("src", img)
-    $("div").removeClass("x-nieobecnosc-nieuspr")
+    $(".x-nieobecnosc-nieuspr").addClass("x-obecnosc")
+    $(".x-nieobecnosc-nieuspr").removeClass("x-nieobecnosc-nieuspr")
+
 })
